fbb8e65f0d8b67651b8a1049bf6488b4709e4e09
User <surbhidongaonkar@gmail.com>
2019-11-20 22:20:43 +0530

    adding prometheus

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -5,6 +5,7 @@ import(
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+// exposes go metrics
 func ServeMetrics(){
 	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(":2112", nil)
